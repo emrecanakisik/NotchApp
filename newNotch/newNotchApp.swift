9ade79cefc7fb4ee7eac9ada4487af291684d575
emrecanakisik <emrecanakisik2004@gmail.com>
2026-03-19 00:54:38 +0300

    Initial Commit

---
+++ newNotch/newNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  newNotchApp.swift
+//  newNotch
+//
+//  Created by Emre Can Akisik on 19/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct newNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
